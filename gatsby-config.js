237c6a70e03a5f4f46daf5c6d43a0eba881e2e3c
Ben Robertson <hi@benrobertson.io>
2020-03-03 14:46:45 -0500

    Add clean script.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,8 @@
-require("dotenv").config({
+require('dotenv').config({
   path: `.env.${process.env.NODE_ENV}`,
-})
+});
+
+console.log(`Environment: ${process.env.NODE_ENV}`);
 
 //configure your agility plugin with environment variables so that
 //your agility api credentials stay secure
@@ -8,7 +10,9 @@ const agilityConfig = {
   guid: process.env.AGILITY_GUID,
   apiKey: process.env.AGILITY_API_KEY,
   isPreview: process.env.AGILITY_API_ISPREVIEW
-}
+};
+
+console.log({ agilityConfig });
 
 /**
  * Configure your Gatsby site with this file.
@@ -18,40 +22,40 @@ const agilityConfig = {
 
 module.exports = {
   siteMetadata: {
-    title: "Agility CMS Gatsby Starter",
+    title: 'Agility CMS Gatsby Starter',
 
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-react-helmet',
     {
       //the name of the plugin
-      resolve: "@agility/gatsby-source-agilitycms", 
+      resolve: '@agility/gatsby-source-agilitycms',
       //the options for our plugin
       options: {
         //your Agility Content Fetch API Guid
-        guid: agilityConfig.guid, 
+        guid: agilityConfig.guid,
         //your Agility Content Fetch API Key
-        apiKey: agilityConfig.apiKey, 
+        apiKey: agilityConfig.apiKey,
         //set this to true if you are using the preview API Key
-        isPreview: agilityConfig.isPreview, 
+        isPreview: agilityConfig.isPreview,
         //set this to true to see expanded traces in the build logs
         debug: false,
         //the languages you want to source content for
         languages: [{
           // The name of the language code
-          name: "English",
+          name: 'English',
           // The actual language code set in Agility CMS
-          code: "en-us", 
+          code: 'en-us',
           // The name to be used in the URL path that represents the current language
-          path: "en"
-        }], 
+          path: 'en'
+        }],
         // The channels you want to include
         channels: [{
           // The reference name for the website channel as it is defined in Agility CMS
-          referenceName: "website"
+          referenceName: 'website'
         }],
-        masterPageTemplate: "./src/AgilityPage.js", //the page template that will be used to render Agility CMS pages
+        masterPageTemplate: './src/AgilityPage.js', //the page template that will be used to render Agility CMS pages
       },
     },
   ],
-}
+};
